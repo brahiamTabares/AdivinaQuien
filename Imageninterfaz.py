e9a9a3feace4879d95c13fb1311e1e9c88b76373
BrahiamTabares <bdtabaresv@uqvirtual.edu.co>
2023-03-15 14:34:42 -0500

    interfaz con texto y botones completa

--- Imageninterfaz.py
+++ Imageninterfaz.py
@@ -28,14 +28,39 @@ miTexto = miFuente.render("Adivina Quien",0,(255,255,0))
 miFuenteSistema = pygame.font.SysFont("Arial",50)
 miTextoSistema = miFuenteSistema.render("Juego",0,(255,255,255))
 
+#configuracion del Botton
+
+BUTTONCOLOR1 = (255,255, 0)
+TEXTBUTTONCOLOR = (0, 0,255)
+x = 200
+y = 400
+ancho = 200
+alto = 100
+rectangulo = pygame.Rect(x, y, ancho, alto)
+
+pygame.draw.rect(window, BUTTONCOLOR1, rectangulo)
+
+fuente = pygame.font.Font(None, 80)
+texto = fuente.render("PLAY", True, TEXTBUTTONCOLOR)
+texto_rect = texto.get_rect(center=rectangulo.center)
 while True:
     window.fill(colorFondo)
     window.blit(ImagenPrincipal, (20, 5))
+    window.blit(texto, texto_rect)
     for evento in pygame.event.get():
         if evento.type==QUIT:
             pygame.quit()
             sys.exit()
+        if evento.type == pygame.MOUSEBUTTONDOWN:
+            if rectangulo.collidepoint(evento.pos):
+                print("Haz hecho clic en el botón")
+
+
     # llamados de los componentes de texto
     window.blit(miTextoSistema,(220,200))
     window.blit(miTexto,(100,250))
+
+    #Botton event
+
+
     pygame.display.update()
